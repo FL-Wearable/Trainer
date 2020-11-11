6b8c4771e236b697e4ad5c6dd07e95e335de7f8b
Pengyuan Zhou <zpymyyn@gmail.com>
2020-11-11 05:08:05 +0200

    Revert "tmp change"
    This reverts commit b052671b0838b03b9d61fe9bee46f71e29b46a2b.

--- Application/src/main/java/fl/wearable/autosport/login/LoginActivity.kt
+++ Application/src/main/java/fl/wearable/autosport/login/LoginActivity.kt
@@ -82,10 +82,7 @@ class LoginActivity : AppCompatActivity(),
             this,
             ViewModelProvider.NewInstanceFactory()
         ).get(LoginViewModel::class.java)
-        val dir = File(applicationContext().filesDir, "sync")
-        if (!dir.exists()) {
-            dir.mkdir()
-        }
+
 
         binding.button.setOnClickListener {
             val baseUrl = binding.url.text.toString()
@@ -110,13 +107,13 @@ class LoginActivity : AppCompatActivity(),
 
         // for transmissions
         binding.send.setOnClickListener {
-            val dir = File(filesDir, "models")
+            val dir = File(filesDir, "sync")
             if (!dir.exists()) {
                 Log.d(TAG, "dir doesn't exist, create it now")
                 dir.mkdir()
             }
 
-            val modelFile = File(dir, "1.pb")
+            val modelFile = File(dir, "model.pt")
             if (!modelFile.exists()) {
                 Log.d(TAG, "no model")
             }
